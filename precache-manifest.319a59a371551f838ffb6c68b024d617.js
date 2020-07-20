3e6367791d520ebe86fba9d470ecfe9f9810a006
thavixt <thavixt@users.noreply.github.com>
2020-07-20 16:51:35 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.319a59a371551f838ffb6c68b024d617.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2e3e1fd4603fc87c8a81183fc49551e",
+    "url": "/twitch-lite/index.html"
+  },
+  {
+    "revision": "07355193c1f84ee7857b",
+    "url": "/twitch-lite/static/css/main.dcda44a5.chunk.css"
+  },
+  {
+    "revision": "d8c3717144df5a04cfa9",
+    "url": "/twitch-lite/static/js/2.89d9f882.chunk.js"
+  },
+  {
+    "revision": "07355193c1f84ee7857b",
+    "url": "/twitch-lite/static/js/main.43248234.chunk.js"
+  },
+  {
+    "revision": "e63bc113a92d76128201",
+    "url": "/twitch-lite/static/js/runtime~main.6e49f2df.js"
+  }
+]);
